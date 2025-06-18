b0181c5c6570fd5da4776bfca7b174e3821ab3ea
David Vaquero <pepesan@gmail.com>
2025-06-18 18:32:15 +0200

    mejoras del ejemplo de csrf

--- src/main/java/com/cursosdedesarrollo/ciberseguridad/controllers/CsrfController.java
+++ src/main/java/com/cursosdedesarrollo/ciberseguridad/controllers/CsrfController.java
@@ -1,19 +1,24 @@
 package com.cursosdedesarrollo.ciberseguridad.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpServletRequest;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 @Controller
 public class CsrfController {
 
     // Carga la vista y añade CsrfToken solo para el formulario seguro
     @GetMapping("/csrf")
-    public String showCsrfDemo(HttpServletRequest request, Model model) {
-        String  token = "blablabla";
+    public String showCsrfDemo(HttpSession session, Model model) {
+        String  token = generarTokenUUID(128);
+        // Almacena el valor bajo la clave "miClave"
+        session.setAttribute("safeCsrf", token);
         model.addAttribute("safeCsrf", token);
         return "csrf_demo";
     }
@@ -22,13 +27,17 @@ public class CsrfController {
     public String submitSafe(
             @RequestParam(name = "data", required = false) String data,
             @RequestParam(name = "csrf_token", required = false) String csrf_token,
-
+            HttpSession session,
             HttpServletRequest request,
             Model model) {
-        if (csrf_token == null) {
+        String token = (String) session.getAttribute("safeCsrf");
+        if (csrf_token == null &&  csrf_token.equals(token)) {
             model.addAttribute("error", "Token invalido");
             return "fallo";
         }
+        token = generarTokenUUID(128);
+        // Almacena el valor bajo la clave "miClave"
+        session.setAttribute("safeCsrf", token);
         model.addAttribute("safeCsrf", csrf_token);
         model.addAttribute("messageSafe", "Formulario seguro recibido: " + data);
         model.addAttribute("dataSafe", data);
@@ -42,5 +51,15 @@ public class CsrfController {
         model.addAttribute("messageUnsafe", "Intento vulnerable con datos: " + data);
         model.addAttribute("dataUnsafe", data);
         return "csrf_demo";
+    }
+
+    public String generarTokenUUID(int numBytes) {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] bytes = new byte[numBytes];
+        secureRandom.nextBytes(bytes);
+        // Codificamos en Base64 sin padding y apto para URLs
+        return Base64.getUrlEncoder()
+                .withoutPadding()
+                .encodeToString(bytes);
     }
 }
